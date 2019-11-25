880cbb2d0b461d345041350ef59d7d27cfec6cc0
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 09:46:27 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  temp_array=[]
+  while array[counter] do
+    element=array[counter]
+    element = element * element
+    temp_array.push(element)
+    counter +=1
+  end
+  array = temp_array
 end
